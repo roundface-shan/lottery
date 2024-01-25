6d1331fb6fca36533e93fea5e9643fd89929225f
roundface_shan <lice2567614@gmail.com>
2024-01-25 16:45:12 +0800

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   foundry.toml
    	modified:   src/Ruffle.sol

--- src/Ruffle.sol
+++ src/Ruffle.sol
@@ -23,15 +23,26 @@
 pragma solidity ^0.8.18;
 
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
 /**
- * @title A sample ruffle contract
+ * @title A sample Raffle contract
  * @author Lice
- * @notice This contract is for creating a sample ruffle
+ * @notice This contract is for creating a sample Raffle
  * @dev Implements Chainlink VRFv2
  */
-contract Ruffle {
+
+// 这个阶段是把基本功能实现，先不写测试，也不写前端
+contract Raffle is VRFConsumerBaseV2 {
     error Raffle__NotEnoughFees();
+    error Raffle__TransferFailed();
+    error Raffle__RaffleNotOpen();
+
+    /** Type Declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
 
     /** State Variables */
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
@@ -45,41 +56,48 @@ contract Ruffle {
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     uint64 private immutable i_subscriptionId;
     uint32 private immutable i_callbackGasLimit;
+    address private s_recentWinner;
+    RaffleState private s_raffleState;
 
     /** Events */
-
-    event EnteredRuffle(address indexed player);
+    event EnteredRaffle(address indexed player);
+    event PickedWinner(address indexed winner);
 
     constructor(
         uint256 entranceFee,
         uint256 interval,
-        VRFCoordinatorV2Interface vrfCoordinator,
+        address vrfCoordinator,
         bytes32 gasLane,
         uint64 subscriptionId,
         uint32 callbackGasLimit
-    ) {
+    ) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
-        i_vrfCoordinator = vrfCoordinator;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
     }
 
-    function enterRuffle() external payable {
+    function enterRaffle() external payable {
         // require(msg.value >= i_entranceFee, "Not enough fees");
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughFees();
         }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__RaffleNotOpen();
+        }
         s_players.push(payable(msg.sender));
-        emit EnteredRuffle(msg.sender);
+        emit EnteredRaffle(msg.sender);
     }
 
     function pickWinner() external {
         if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert();
         }
+        s_raffleState = RaffleState.CALCULATING;
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane, // kay hash
             i_subscriptionId, // 8843
@@ -87,6 +105,25 @@ contract Ruffle {
             i_callbackGasLimit, // 返回时消耗的gas上限
             NUM_WORDS // 返回几个数字
         );
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomness
+    ) internal override {
+        uint256 indexOfWinner = randomness[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+        s_recentWinner = winner;
+        s_raffleState = RaffleState.OPEN;
+
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit PickedWinner(winner);
     }
 
     /** Getter Functions */
