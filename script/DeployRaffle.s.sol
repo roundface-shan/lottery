5c6dcb421f1642d2dadab25cdc9ec5c3493a5526
roundface_shan <lice2567614@gmail.com>
2024-02-05 15:01:41 +0800

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   script/DeployRaffle.s.sol
    	new file:   script/HelperConfig.s.sol
    	renamed:    src/Ruffle.sol -> src/Raffle.sol

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,10 @@
+//SPDX-Licence-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
+
+contract DepolyRaffle is Script {
+    function run() external returns (Raffle) {}
+}
---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,28 @@
+// SPDX-licence-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint64 subscriptionId;
+        uint32 callbackGasLimit;
+    }
+
+    function getSpoliaConfig() external view returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+                gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c, // keyHsah
+                subscriptionId: 8843,
+                callbackGasLimit: 200000
+            });
+    }
+}
