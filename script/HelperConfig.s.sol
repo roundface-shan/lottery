ace5d9d46cf9dad87340063933aef67b0e55dfa7
roundface_shan <lice2567614@gmail.com>
2024-02-21 14:46:07 +0800

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   script/DeployRaffle.s.sol
    	modified:   script/HelperConfig.s.sol

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,8 +1,9 @@
 // SPDX-licence-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -14,7 +15,17 @@ contract HelperConfig is Script {
         uint32 callbackGasLimit;
     }
 
-    function getSpoliaConfig() external view returns (NetworkConfig memory) {
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
         return
             NetworkConfig({
                 entranceFee: 0.01 ether,
@@ -22,7 +33,33 @@ contract HelperConfig is Script {
                 vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c, // keyHsah
                 subscriptionId: 8843,
-                callbackGasLimit: 200000
+                callbackGasLimit: 500000 // call 我的fullfill方法
+            });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.vrfCoordinator != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        uint96 baseFee = 0.1 ether;
+        uint96 gasPriceLink = 1e9; // 1 Gwei of $Link
+
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vrfCoordinatorMock = new VRFCoordinatorV2Mock(
+            baseFee,
+            gasPriceLink
+        );
+        vm.stopBroadcast();
+
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: address(vrfCoordinatorMock),
+                gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c, // keyHsah
+                subscriptionId: 0, // 0 means no subscription, but eventually we will have one
+                callbackGasLimit: 500000 // call 我的fullfill方法
             });
     }
 }
